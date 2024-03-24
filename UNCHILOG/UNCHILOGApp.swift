55bfcfdd62dfff6ac8673704121c8a7e884d0393
amefure <shachi10969@gmail.com>
2024-03-24 13:40:59 +0900

    プロジェクト作成

--- UNCHILOG/UNCHILOGApp.swift
+++ UNCHILOG/UNCHILOGApp.swift
@@ -6,15 +6,26 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import GoogleMobileAds
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
+    }
+}
+
 
 @main
 struct UNCHILOGApp: App {
-    let persistenceController = PersistenceController.shared
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
