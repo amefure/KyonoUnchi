c96ba8f57ccd2fcb5050f6393fd878873bf7e122
amefure <shachi10969@gmail.com>
2024-04-02 23:37:48 +0900

    カレンダーViewのUI調整

--- UNCHILOG/View/Calendar/PoopCalendarView.swift
+++ UNCHILOG/View/Calendar/PoopCalendarView.swift
@@ -10,59 +10,63 @@ import SwiftUI
 struct PoopCalendarView: View {
     @ObservedObject private var poopViewModel = PoopViewModel.shared
     @ObservedObject private var rootEnvironment = RootEnvironment.shared
+    
+    private let columns = Array(repeating: GridItem(spacing: 0), count: 7)
     var body: some View {
         VStack(spacing: 0) {
             
             YearAndMonthSelectionView()
             
-            LazyVGrid(columns: Array(repeating: GridItem(), count: 7), spacing: 0) {
+            HStack {
+                Image(systemName: "swift")
+                    .font(.system(size: 25))
+                    .frame(width: 50, height: 50)
+                    .foregroundStyle(.indigo)
+                    .background(.exThema)
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
+                    .shadow(color: .gray, radius: 3, x: 2, y: 2)
+                
+                Spacer()
+                
+                ZStack {
+                    RoundChatView()
+                        .fill(Color.indigo)
+                        .frame(width: 250, height: 50)
+                        .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+                    Text("うんちが５日出てないよ")
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .offset(x: 10)
+                }
+                
+                Spacer()
+                
+                NavigationLink {
+                    PoopInputView(theDay: Date())
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.system(size: 25))
+                        .frame(width: 50, height: 50)
+                        .foregroundStyle(.indigo)
+                        .background(.exThema)
+                        .clipShape(RoundedRectangle(cornerRadius: 50))
+                        .shadow(color: .gray, radius: 3, x: 2, y: 2)
+                }
+            }.padding(20)
+
+            
+            LazyVGrid(columns: columns, spacing: 0) {
                 ForEach(rootEnvironment.dayOfWeekList, id: \.self) { week in
                     Text(week.shortSymbols)
                 }
             }
             
             ScrollView {
-                
-                LazyVGrid(columns: Array(repeating: GridItem(spacing: 0), count: 7), spacing: 0) {
+                LazyVGrid(columns: columns, spacing: 0) {
                     ForEach($rootEnvironment.currentDates) { theDay in
                         TheDayView(theDay: theDay, poops: poopViewModel.poops)
                     }
-                }
-                
-                Spacer()
-                
-                
-                HStack {
-                    
-                    Spacer()
-                    
-                    
-                    ZStack {
-                        RoundChatView()
-                            .fill(Color.gray)
-                            .frame(width: 250, height: 50)
-                            .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
-                        Text("うんちが５日出てないよ")
-                            .foregroundStyle(.white)
-                            .offset(x: 10)
-                    }
-                    
-                    
-                    Spacer()
-                    
-                    NavigationLink {
-                        PoopInputView(theDay: Date())
-                    } label: {
-                        Image(systemName: "plus")
-                            .font(.system(size: 25))
-                            .frame(width: 50, height: 50)
-                            .foregroundStyle(.indigo)
-                            .background(.exThema)
-                            .clipShape(RoundedRectangle(cornerRadius: 50))
-                            .shadow(color: .gray, radius: 3, x: 2, y: 2)
-                    }.padding(.trailing, 20)
-                }.padding(.top, 10)
-                    .padding(.bottom, 25)
+                }.padding(.bottom, 25)
             }
         }
     }
