9294b0e14447c01078e58f7f7181b4be4c921a69
amefure <shachi10969@gmail.com>
2024-05-03 22:08:47 +0900

    watchターゲットの追加

---
+++ UNCHILOGWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UNCHILOGWatch Watch App
+//
+//  Created by t&a on 2024/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UNCHILOGWatch Watch App/UNCHILOGWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNCHILOGWatchApp.swift
+//  UNCHILOGWatch Watch App
+//
+//  Created by t&a on 2024/05/03.
+//
+
+import SwiftUI
+
+@main
+struct UNCHILOGWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
