1e4a51e78ba2080827fb54b35e6810c3d197aa7f
amefure <shachi10969@gmail.com>
2026-04-16 22:05:09 +0900

    テストビルドの追加

---
+++ UNCHILOGTests/UNCHILOGTests.swift
@@ -0,0 +1,16 @@
+//
+//  UNCHILOGTests.swift
+//  UNCHILOGTests
+//
+//  Created by t&a on 2026/04/16.
+//
+
+import Testing
+
+struct UNCHILOGTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
